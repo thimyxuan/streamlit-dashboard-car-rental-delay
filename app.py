9186675fa9c563fc6c1cdbb1d4bf7c10ed60ebbb
Thi My Xuân Nguyen <thimyxuan.nguyen@gmail.com>
2024-03-10 20:40:03 +0100

    ajout retards impactants et resume

--- app.py
+++ app.py
@@ -27,12 +27,13 @@ st.write(
     }
 
     [data-testid="stMetricDelta"], .st-emotion-cache-wnm74r, .e1i5pmia0 {
-        color: gold !important;
+        color: darksalmon !important;
         font-size: .9rem !important;
     }
 
     h1 {
         font-size: 2.2rem;
+        padding-bottom: 0;
     }
 
     h2 {
@@ -66,6 +67,15 @@ st.write(
     .st-emotion-cache-z5fcl4 {
         padding-top: 1rem;
         padding-bottom: 6.5rem;
+    }
+
+    .st-emotion-cache-pe5sya > p, .e1nzilvr5 > p,
+    .st-emotion-cache-pe5sya > ul > li > p, .e1nzilvr5 > ul > li > p {
+        color: rgba(250, 250, 250, 0.8)
+    }
+
+    .st-emotion-cache-pe5sya > p > em, .e1nzilvr5 > p > em {
+        color: rgba(250, 250, 250, 0.6);
     }
     
     </style>
@@ -123,14 +133,14 @@ with col1:
             orientation = 'v',
             offsetgroup = 0,
             marker = dict(
-                color = 'dodgerblue')
+                color = 'lightsalmon')
             ))
     fig.update_layout(height=400)
     fig.update_traces(textfont_size=15)
     st.plotly_chart(fig, use_container_width=True)
 
 with col3:
-    colors = ['aliceblue', 'dodgerblue']
+    colors = ['lightskyblue', 'dodgerblue']
     labels = ['mobile','connect']
     values = [17002, 4307]
     fig = go.Figure(data=[go.Pie(labels=labels, values=values, pull=[0, 0.1])])
@@ -154,14 +164,13 @@ with col4:
             base = 0,
             orientation = 'v',
             marker = dict(
-                color = 'gold')
+                color = 'salmon')
             ))
     fig.update_traces(textfont_size=15)
     fig.update_layout(height=400)
     st.plotly_chart(fig, use_container_width=True)
 
 with col6:
-    colors = ['aliceblue', 'dodgerblue']
     labels = ['mobile','connect']
     values = [1028, 813] 
     fig = go.Figure(data=[go.Pie(labels=labels, values=values, pull=[0, 0.1])])
@@ -199,40 +208,39 @@ with col4:
 # Tight rentals revenue
 
 col0, col1, col2, col3, col4, col5, col6 = st.columns([.5,.1,.5,.1,.5,.1,.5])
-colors = ['aliceblue', 'dodgerblue']
 
 with col0:
-    st.subheader(':moneybag:- Revenus générés')
+    st.subheader(':moneybag: Revenus générés')
     labels = ['tous','délai serré']
     values = [21309-7255, 7255]
     fig = go.Figure(data=[go.Pie(labels=labels, values=values, pull=[0, 0.05])])
     fig.update_layout(height=310)
     fig.update_traces(hoverinfo='label+value', textinfo='percent', textfont_size=15,
-                  marker=dict(colors=colors, line=dict(color='#000000', width=2)))
+                  marker=dict(colors=['pink', 'salmon'], line=dict(color='#000000', width=2)))
     st.plotly_chart(fig, use_container_width=True)
 
 with col2:
-    st.subheader(':oncoming_automobile: - Parc de voitures')
+    st.subheader(':oncoming_automobile: Parc de voitures')
     labels = ['tous','délai serré']
     values = [8142-1087, 1087]
     fig = go.Figure(data=[go.Pie(labels=labels, values=values, pull=[0, 0.05])])
     fig.update_layout(height=310)
     fig.update_traces(hoverinfo='label+value', textinfo='percent', textfont_size=12.5,
-                  marker=dict(colors=colors, line=dict(color='#000000', width=2)))
+                  marker=dict(colors=['pink', 'salmon'], line=dict(color='#000000', width=2)))
     st.plotly_chart(fig, use_container_width=True)
 
 with col4:
-    st.subheader(':birthday: - Part des revenus')
+    st.subheader(':birthday: Part des revenus')
     labels = ['tous','délai serré']
     values = [72, 28]
     fig = go.Figure(data=[go.Pie(labels=labels, values=values, pull=[0, 0.05])])
     fig.update_layout(height=310)
     fig.update_traces(hoverinfo='label+value', textinfo='percent', textfont_size=15,
-                  marker=dict(colors=colors, line=dict(color='#000000', width=2)))
+                  marker=dict(colors=['pink', 'salmon'], line=dict(color='#000000', width=2)))
     st.plotly_chart(fig, use_container_width=True)
 
 with col6:
-    st.subheader(':key: - Méthode check-in')
+    st.subheader(':key: Méthode check-in')
     labels = ['mobile','connect']
     values = [1028, 813]
     fig = go.Figure(data=[go.Pie(labels=labels, values=values, pull=[0, 0.05])])
@@ -265,9 +273,17 @@ def load_data():
     return data
 data = load_data()
 
+@st.cache_data
+def load_impacting_delay():
+    data = pd.read_csv('src/impacting_delay.csv')
+    return data
+df = load_impacting_delay()
+impacting_delay = df[df['previous_delay_hours'] <= 12]
+
 col0, col1 = st.columns([5,3])
 
 with col0:
+    st.subheader('Diminution des réservations suivant le délai')
     fig = px.bar(data, x='seuil_en_heure', y=['reservations','annulations'], barmode='group')
     fig.update_layout(bargap=0,
                  xaxis_title="délai en heure", 
@@ -279,26 +295,18 @@ with col0:
                  ticktext = [i for i in range(0,13,1)])
     st.plotly_chart(fig, use_container_width=True)
     st.caption("""
-        :arrow_right: Le nombre de réservations diminue rapidement au départ, puis à partir d'un délai de 5 heures il diminue moins vite. Définir un seuil à 5 heures permettrait de conserver 735 réservations et de descendre à moins de 100 annulations.
+        - Le nombre de réservations diminue rapidement au départ, puis à partir d'un délai de 5 heures il diminue moins vite. 
         
-        :arrow_right: Si l'on désire minimiser les pertes, définir un seuil à 3 heures permettrait de conserver plus de la moitié des réservations et d'avoir 100 annulations en moins (130 au lieu de 230 annulations).
+        - Définir un seuil à 5 heures permettrait de conserver 735 réservations et de descendre à moins de 100 annulations. Les revenus des loueurs concernés descendraient à 28.8% des revenus totaux.
+        
+        - Si l'on désire minimiser les pertes, définir un seuil à 2,5 heures permettrait de conserver 1000 réservations et d'avoir 140 annulations au lieu de 230. Les revenus des loueurs concernés seraient maintenus à 30% des revenus totaux.
 
-        :arrow_right: Les réservations avec un délai court s'effectuant principalement avec des voitures connect, il serait judicieux de tester la fonctionnalité sur ce type de véhicule en particulier.
+        - Cependant on observe que la majorité des retards impactants se situent dans un délai de 0 à 4 heures. Par conséquent le seuil devrait être de **4 heures minimum**. Dans ce cas on conserverait 840 réservations (perte de 1000 réservations) et le nombre d'annulations descendrait à 110 contre 230. Les revenus des loueurs concernés seraient de 29.3% des revenus totaux.
+
+        - Au-delà de 12 heures il n'y aurait plus de réservations à délai court. Les loueurs concernés ne représenteraient plus qu'un quart des revenus totaux alors qu'ils représentaient plus du tiers au départ.
+
+        - Les réservations avec un délai court s'effectuant principalement avec des **voitures Connect**, il serait judicieux de tester la fonctionnalité sur ce type de véhicule en particulier.
     """)
-
-with col1:
-    st.subheader('Diminution des réservations suivant le délai')
-    fig = px.area(data, x='seuil_en_heure', y=['reservations'])
-    fig.update_layout(bargap=0,
-                    xaxis_title="délai en heure", 
-                    yaxis_title="nombre", 
-                    height=300)
-    fig.update_xaxes(tickangle=0,
-                    tickmode = 'array',
-                    tickvals = [i for i in range(0,13,1)],
-                    ticktext = [i for i in range(0,13,1)])
-    fig.update_traces(marker_color=colors)
-    st.plotly_chart(fig, use_container_width=True)
 
 with col1:
     st.subheader('Diminution des annulations suivant le délai')
@@ -313,6 +321,35 @@ with col1:
                     ticktext = [i for i in range(0,13,1)])
     st.plotly_chart(fig, use_container_width=True)
 
+with col1:
+    reservations = ['1840', '1000', '840', '735', '0']
+    annulations = ['230', '140', '110', '< 100', '0']
+    part_revenus = ['34 %', '30,3 %', '29,3 %', '28,8 %', '25,4 %']
+    delai = ['0 heure','2,5 heures', '4 heures', '5 heures', '12 heures']
+    columns = ['délai', 'réservations', 'annulations', 'part revenus']
+    threshold_df = pd.DataFrame(list(zip(delai, reservations, annulations, part_revenus)), columns=columns)
+    st.table(threshold_df)
+
+with col1:
+    st.subheader('Distribution des retards impactants')
+    fig = px.histogram(impacting_delay, x="previous_delay_hours", nbins=9, color_discrete_sequence=['indianred'])
+    fig.update_layout(
+            bargap=0,
+            xaxis_title="retard en heure",
+            yaxis_title="nombre", 
+            height=360,
+            xaxis = dict(
+                tickmode = 'linear',
+                tick0 = 0,
+                dtick = 1
+            )
+    )
+    fig.update_xaxes(tickangle=0,
+                    tickmode = 'array',
+                    tickvals = [i for i in range(0,13,1)],
+                    ticktext = [i for i in range(0,13,1)])
+    st.plotly_chart(fig, use_container_width=True)
+    
 
 # Footer
 st.text("""
